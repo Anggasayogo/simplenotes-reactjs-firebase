c173d9095acd151473166b6afb4e6320046ab721
anggasayogo <anggasayogosm@gmail.com>
2020-08-15 12:11:04 +0700

    Setup Redux

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import logo from '../../../assets/img/logo/logo.svg';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from '../Dashboard';
 import Login from '../Login';
 import Register from '../Register';
+import './App.css';
+import {store} from '../../../config/redux';
 
 function App() {
+
   return (
-    <Router>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Route path="/" exact component={Dashboard}/>
-        <Route path="/login" exact component={Login}/>
-        <Route path="/register" exact component={Register}/>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Route path="/" exact component={Dashboard}/>
+          <Route path="/login" exact component={Login}/>
+          <Route path="/register" exact component={Register}/>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- src/containers/pages/Login/index.js
+++ src/containers/pages/Login/index.js
@@ -1,15 +1,18 @@
 import React,{Component} from 'react';
+import { connect } from 'react-redux';
 
 
 class Login extends Component{
     render(){
         return(
             <div>
-                <p>Login Page</p>
+                <p>Login Page {this.props.popupProps}</p>
                 <button>Go To Register</button>
             </div>
         )
     }
 }
-
-export default Login;
+const reduxState = (state)=>({
+    popupProps : state.popup
+})
+export default connect(reduxState,null)(Login);
