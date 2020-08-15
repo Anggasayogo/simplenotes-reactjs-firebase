14bcb6087e79b117c319d51db6603bc6c4cc84ad
anggasayogo <anggasayogosm@gmail.com>
2020-08-15 08:42:49 +0700

    Integrasi Firebase dengan Project ReactJS

---
+++ src/config/firebase/index.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBbJRvggm7pSlk4XT-YCzP--hDzDwaNCOE",
+    authDomain: "simplenotes-firebase.firebaseapp.com",
+    databaseURL: "https://simplenotes-firebase.firebaseio.com",
+    projectId: "simplenotes-firebase",
+    storageBucket: "simplenotes-firebase.appspot.com",
+    messagingSenderId: "509943450111",
+    appId: "1:509943450111:web:3204fd47290bf94ac03fca"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
